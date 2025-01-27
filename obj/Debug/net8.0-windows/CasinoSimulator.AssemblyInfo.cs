d3fc4f80c933fe25110bf95a4dd2cc18ee95640c
Oliverpartequattro <oliverroncz2@gmail.com>
2025-01-27 10:28:35 +0100

    tervek #4 #12 #13

--- obj/Debug/net8.0-windows/CasinoSimulator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CasinoSimulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da0761a77e7bc87a6abdf8ea73a4b59f6d37b34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae2b52c141cd52605e483b60829e5f4515167f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
