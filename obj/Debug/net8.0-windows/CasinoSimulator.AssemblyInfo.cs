9e8fced226d445c069775d51c77348468c27b768
HUNBenjamin <reinhardt.benjamin@students.jedlik.eu>
2025-01-29 10:14:33 +0100

    test

--- obj/Debug/net8.0-windows/CasinoSimulator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CasinoSimulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae2b52c141cd52605e483b60829e5f4515167f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbb54272babe3803f0db42c71ea034060fffe473")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
