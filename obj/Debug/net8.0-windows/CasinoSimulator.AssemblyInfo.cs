80b73eed21780b248467045eef8bd8421b91d843
EloZalan <elo.zalan.laszlo@students.jedlik.eu>
2025-01-29 10:06:12 +0100

    first

--- obj/Debug/net8.0-windows/CasinoSimulator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CasinoSimulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae2b52c141cd52605e483b60829e5f4515167f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b9d8ef490014655b6a9b9bbc349a08a399b611")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasinoSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
