136a732205d6f4c5448cc6259af01a50ca1f5f75
Élő Zalán <90603993+EloZalan@users.noreply.github.com>
2025-02-11 12:58:51 +0100

    chickenCross game from xaml.cs ready #9

--- ChickenCross.xaml.cs
+++ ChickenCross.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,11 +27,14 @@ namespace CasinoSimulator
         private Rectangle chicken;
         private double chickenSpeed = 800/20;
         private DispatcherTimer gameTimer;
+        private double x = 1;
 
         public ChickenCross()
         {
             InitializeComponent();
+            BackgroundCreation();
             InitializeGame();
+
         }
 
         private void InitializeGame()
@@ -46,7 +50,7 @@ namespace CasinoSimulator
 
             // Set initial position of the chicken
             Canvas.SetLeft(chicken,100 + (800/20/4) - (800 / 20));
-            Canvas.SetTop(chicken, gameCanvas.Height/2);
+            Canvas.SetTop(chicken, Height/2);
             gameCanvas.Children.Add(chicken);
 
             // Set up a game timer to update the game state
@@ -73,6 +77,39 @@ namespace CasinoSimulator
             bool a = x < (gameCanvas.Width - chicken.Width);
             if (e.Key == Key.Right && x < (gameCanvas.Width - 100) - chicken.Width)
                 Canvas.SetLeft(chicken, x + chickenSpeed);
+        }
+        private void BackgroundCreation()
+        {
+            for (int i = 0; i < 22; i++)
+            {
+                myGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                x = (x + (i * 0.1))/1.2;
+                var lable = new Label()
+                {
+                    Content = Math.Round(x, 2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+                var border = new Border() { Background = Brushes.DimGray };
+                var borderGreen = new Border() { Background = Brushes.DarkOliveGreen };
+                if ((i % 2) == 0){
+                    Grid.SetColumn(border, i);
+                    myGrid.Children.Add(border);
+                }
+                if (i == 0 || i == 21)
+                {
+                    Grid.SetColumn(borderGreen, i);
+                    myGrid.Children.Add(borderGreen);
+                    myGrid.ColumnDefinitions[i].Width = new GridLength(100);
+                }
+                if (i > 0 && i < 21 )
+                {
+                    lable.Foreground = Brushes.White;
+                    Grid.SetColumn(lable, i);
+                    myGrid.Children.Add(lable);
+                }
+                
+            }
         }
 
     }
